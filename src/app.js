894123d10489e02074a58d1236d2d9447db1e6f9
messend <aquilmessen@gmail.com>
2025-12-04 21:11:24 +0000

    mazo de cartas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const iconos = ["♦", "♥", "♠", "♣"];
+  const numerosYLetras = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+  
+  const iconoRandom = iconos[Math.floor(Math.random() * iconos.length)];
+  const numeroRandom = numerosYLetras[Math.floor(Math.random() * numerosYLetras.length)]
+
+  document.querySelector(".icono-top").innerHTML = iconoRandom
+  document.querySelector(".numerosyletra").innerHTML = numeroRandom
+  document.querySelector(".icono-bottom").innerHTML = iconoRandom
+
+  if (iconoRandom === "♥" || iconoRandom === "♦" ) {
+    document.querySelector(".icono-top").style.color = "red";
+    document.querySelector(".icono-bottom").style.color = "red";
+  } else {
+    document.querySelector(".icono-top").style.color = "black";
+    document.querySelector(".icono-bottom").style.color = "black";
+  }
 };
